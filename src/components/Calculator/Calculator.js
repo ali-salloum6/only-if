435d4830dc663a527fc3815896f8c6f238e41da4
Ali <ali.salloum.gh@gmail.com>
2022-09-19 20:07:56 +0300

    added the title image

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,7 +1,12 @@
 import "./Calculator.css";
+import title from "../../assets/title_image.png";
 
 function Calculator() {
-  return <div id="calc"></div>;
+  return (
+    <div id="calc">
+      <img id="title" src={title} alt="Only If" />
+    </div>
+  );
 }
 
 export default Calculator;
