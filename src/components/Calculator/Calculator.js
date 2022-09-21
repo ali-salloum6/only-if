3f947d65394e2f7fdab174f427340ad8782bb3af
Ali <ali.salloum.gh@gmail.com>
2022-09-21 16:55:09 +0300

    added profit state and setup calculateProfit function

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,7 +1,8 @@
 import "./Calculator.css";
 import logo from "../../assets/logo_transparent.png";
 import InputSection from "../InputSection/InputSection";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import calculateProfit from "../../utils/calculateProfit";
 
 function Calculator() {
   const [input, setInput] = useState({
@@ -9,6 +10,7 @@ function Calculator() {
     currency: "BTC",
     time: "hour",
   });
+  const [profit, setProfit] = useState(null);
 
   function handleInputChange(event) {
     const target = event.target;
@@ -21,6 +23,10 @@ function Calculator() {
     });
   }
 
+  useEffect(() => {
+    setProfit(calculateProfit(input));
+  }, [input]);
+
   return (
     <div id="calc">
       <div id="title-section">
@@ -29,7 +35,7 @@ function Calculator() {
       </div>
       <InputSection input={input} handleInputChange={handleInputChange} />
       <h3 id="result" className="wish">
-        I would have earned ___$
+        I would have earned {profit ? profit : "___"}$
       </h3>
     </div>
   );
